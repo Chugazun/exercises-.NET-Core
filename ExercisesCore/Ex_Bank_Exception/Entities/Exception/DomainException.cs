5f663f4a1b974cc9afb92693c501a4e3552ae36d
Samuel Silva <samuelrodolfos@gmail.com>
2020-01-07 16:27:26 -0300

    Exercicios dia 07/01

---
+++ ExercisesCore/Ex_Bank_Exception/Entities/Exception/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercisesCore.Ex_Bank_Exception.Entities.Exception
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- ExercisesCore/Ex_Taxes_Abstract/Ex_Taxes_Abstract.cs
+++ ExercisesCore/Ex_Taxes_Abstract/Ex_Taxes_Abstract.cs
@@ -7,44 +7,44 @@ namespace ExercisesCore.Ex_Taxes_Abstract
 {
     class Ex_Taxes_Abstract
     {
-        static void Main(string[] args)
-        {
-            List<Body> taxpayers = new List<Body>();
+        //static void Main(string[] args)
+        //{
+        //    List<Body> taxpayers = new List<Body>();
 
-            Console.Write("Enter the number of employees: ");
-            int taxPayersNum = int.Parse(Console.ReadLine());
+        //    Console.Write("Enter the number of employees: ");
+        //    int taxPayersNum = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < taxPayersNum; i++)
-            {
-                Console.Write($"\nTax payer {i + 1} Data:\nIndividual or Company (i/c)? ");
-                char bodyType = char.Parse(Console.ReadLine());
+        //    for (int i = 0; i < taxPayersNum; i++)
+        //    {
+        //        Console.Write($"\nTax payer {i + 1} Data:\nIndividual or Company (i/c)? ");
+        //        char bodyType = char.Parse(Console.ReadLine());
 
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
+        //        Console.Write("Name: ");
+        //        string name = Console.ReadLine();
 
-                Console.Write("Annual income: ");
-                double annualIncome = double.Parse(Console.ReadLine());
+        //        Console.Write("Annual income: ");
+        //        double annualIncome = double.Parse(Console.ReadLine());
 
-                if (bodyType == 'i')
-                {
-                    Console.Write("Health expenditures: ");
-                    taxpayers.Add(new IndividualBody(name, annualIncome, double.Parse(Console.ReadLine())));
-                }
-                else
-                {
-                    Console.Write("Number of employees: ");
-                    taxpayers.Add(new CompanyBody(name, annualIncome, int.Parse(Console.ReadLine())));
-                }
-            }
-            Console.WriteLine("\nTaxes paid");
+        //        if (bodyType == 'i')
+        //        {
+        //            Console.Write("Health expenditures: ");
+        //            taxpayers.Add(new IndividualBody(name, annualIncome, double.Parse(Console.ReadLine())));
+        //        }
+        //        else
+        //        {
+        //            Console.Write("Number of employees: ");
+        //            taxpayers.Add(new CompanyBody(name, annualIncome, int.Parse(Console.ReadLine())));
+        //        }
+        //    }
+        //    Console.WriteLine("\nTaxes paid");
 
-            double totalTaxes = 0;
-            foreach (Body item in taxpayers)
-            {
-                Console.WriteLine(item + item.Taxes().ToString("F2"));
-                totalTaxes += item.Taxes();
-            }
-            Console.WriteLine("\nTotal taxes: $ " + totalTaxes.ToString("F2"));
-        }
+        //    double totalTaxes = 0;
+        //    foreach (Body item in taxpayers)
+        //    {
+        //        Console.WriteLine(item + item.Taxes().ToString("F2"));
+        //        totalTaxes += item.Taxes();
+        //    }
+        //    Console.WriteLine("\nTotal taxes: $ " + totalTaxes.ToString("F2"));
+        //}
     }
 }
